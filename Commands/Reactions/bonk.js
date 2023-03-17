2535bb1b7ed365adca021b840022fe711c3353c0
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:12:50 +0530

    Update bonk.js

--- Commands/Reactions/bonk.js
+++ Commands/Reactions/bonk.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "To bullyfor user",
   category: "Reaction",
   usage: `bonk @user`,
-  react: "🍁",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/bonk`);
     try {
