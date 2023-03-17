c5f8337b5db9bbea8fdaf2c2ddd12188cafdb17e
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:18:00 +0530

    Update yeet.js

--- Commands/Reactions/yeet.js
+++ Commands/Reactions/yeet.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "For Yeet",
   category: "Reaction",
   usage: `yeet @user`,
-  react: "🍁",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/yeet`);
     try {
