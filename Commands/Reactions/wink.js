bbdc81e5d6259635dde2e59836b91489ddaba5cb
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:17:43 +0530

    Update wink.js

--- Commands/Reactions/wink.js
+++ Commands/Reactions/wink.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "To wink for",
   category: "Reaction",
   usage: `wink @user`,
-  react: "🍁",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/wink`);
     try {
