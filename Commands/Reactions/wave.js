35816185f95452da66895355e4d5cd1e9e4d83cd
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:17:28 +0530

    Update wave.js

--- Commands/Reactions/wave.js
+++ Commands/Reactions/wave.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "To wave for user",
   category: "Reaction",
   usage: `wave @user`,
-  react: "🍁",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/wave`);
     try {
