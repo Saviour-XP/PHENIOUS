27143292f3422f23281e97b027be5ea0f04cede0
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:12:18 +0530

    Update behappy.js

--- Commands/Reactions/behappy.js
+++ Commands/Reactions/behappy.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "make happy someone",
   category: "Reaction",
   usage: `behappy @user`,
-  react: "🌝",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args, mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/happy`);
     try {
