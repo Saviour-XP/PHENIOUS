b34b887393f55dc8aae1f8367538ce2301361ad3
hope001 <116964452+hope001@users.noreply.github.com>
2023-03-17 19:12:32 +0530

    Update bite.js

--- Commands/Reactions/bite.js
+++ Commands/Reactions/bite.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "To bite for user",
   category: "Reaction",
   usage: `bite @user`,
-  react: "🍁",
+  react: "❄️",
   start: async (Miku, m, { text, prefix, args,mentionByTag }) => {
     var pat = await fetchJson(`https://api.waifu.pics/sfw/bite`);
     try {
